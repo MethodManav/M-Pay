3d6b3b0ea9113e49072a1a4a6cf2c22b38c1f20c
Behera Manav <manavbehera123@gmail.com>
2024-10-17 22:11:42 +0530

    Configured and packages

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <div className="text-slate-300">Manav</div>;
+"use client"
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Appbar } from "@repo/ui/appbar";
+
+export default function Page(): JSX.Element {
+  const session = useSession();
+  return (
+   <div>
+      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
+   </div>
+  );
 }
